b7c978005021737fb4687361baecb0701d75c90c
MuhammadTahaAzam1997 <muhammadtahaazam@gmail.com>
2018-08-01 13:50:57 +0500

    Inheritance overloading in Java

---
+++ src/InheritenceAndOverloading/InheritenceAndOverloding.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InheritenceAndOverloading;
+
+/**
+ *
+ * @author Muhammad Taha Azam
+ */
+
+
+class Taha{
+public static void main(String ar[]){
+
+}
+}
+
+
+    
+
+   
